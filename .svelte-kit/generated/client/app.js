11a87a1ddfeb31931ee81094ef004d2483e92702
Furqan Ahmed <98402746+furqanahmed1872@users.noreply.github.com>
2024-08-24 15:58:37 +0500

    ok

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,32 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
+];
+
+export const server_loads = [2];
+
+export const dictionary = {
+		"/": [3],
+		"/staff-member": [4,[2]],
+		"/staff-member/create": [~5,[2]],
+		"/test": [6],
+		"/update": [7],
+		"/[id]": [~8]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/staff-member/create/+page.svelte";
