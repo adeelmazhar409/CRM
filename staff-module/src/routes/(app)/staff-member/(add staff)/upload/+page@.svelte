145cfb3c2481f13530e9cb8e4ce76ce648a8dc56
Your Name <your.email@example.com>
2024-08-30 21:42:20 +0500

    ok

--- staff-module/src/routes/(app)/staff-member/(add staff)/upload/+page@.svelte
+++ staff-module/src/routes/(app)/staff-member/(add staff)/upload/+page@.svelte
@@ -37,22 +37,24 @@
 
 <form method="POST" enctype="multipart/form-data">
     <div class="flex flex-col items-center w-full">
-        <div class="font-sans grid grid-cols-3 items-center w-screen border-b">
-            <button
-                type="button"
-                class="cursor-pointer"
-                on:click={() => goto('/staff-member')}
-            >
+        <!-- top for desktop -->
+        <div
+            class="font-sans hidden sm:grid grid-cols-4 md:grid-cols-3 items-center w-full border-b"
+        >
+            <button type="button" on:click={() => goto('/staff-member')}>
                 <img
                     src="/icons/cross.svg"
                     alt="Close"
-                    class="mx-3 my-5 w-8 h-8"
+                    class="mx-3 my-5 md:w-8 md:h-8"
                 />
             </button>
-            <h2 class="text-center font-semibold text-2xl leading-8">
-                Update Details
+
+            <h2
+                class=" mx-auto font-semibold md:text-2xl text-lg leading-8 col-span-2 md:col-span-1"
+            >
+                Add Staff Member
             </h2>
-            <div class="flex justify-end">
+            <div class="hidden md:flex justify-end">
                 <button
                     type="submit"
                     class="bg-purple-600 text-white w-16 p-2 rounded-lg mx-5 my-4"
@@ -61,17 +63,50 @@
                 </button>
             </div>
         </div>
+        <!-- top for mobile -->
+        <div class="flex flex-col sm:hidden w-full py-1 border-b-2">
+            <div class="font-sans grid grid-cols-4 md:grid-cols-3 items-center">
+                <button type="button" on:click={() => goto('/staff-member')}>
+                    <img
+                        src="/icons/cross.svg"
+                        alt="Close"
+                        class="m-1 w-10 h-10"
+                    />
+                </button>
 
-        <div class=" flex w-1/3 my-10 flex-col">
-            <ProgressBar bind:width />
+                <h2
+                    class=" mx-auto font-semibold md:text-2xl text-lg leading-8 col-span-2 md:col-span-1"
+                >
+                    Add Staff Member
+                </h2>
+                <div class="hidden md:flex justify-end">
+                    <button
+                        type="submit"
+                        class="bg-purple-600 text-white w-16 p-2 rounded-lg mx-5"
+                    >
+                        Next
+                    </button>
+                </div>
+            </div>
+            <div class="mx-4">
+                <ProgressBar bind:width />
+            </div>
+        </div>
 
-            <div class=" py-10">
+        <div class="px-4 my-2 w-full sm:w-1/2 lg:w-2/5">
+            <div class="hidden sm:block md:mt-3 xl:mt-6 py-4">
+                <ProgressBar bind:width />
+            </div>
+           
+           
+
+            <div class=" py-6 md:py-10">
                 <div class="text-gray-700 text-xl font-bold leading-28">
                     Add profile Photo
                 </div>
 
                 <div
-                    class="font-normal text-lg leading-7 text-gray-700 justify-between py-2"
+                    class="font-normal text-lg leading-7 text-gray-700 justify-between py-4"
                 >
                     You can add a photo for Alfonso which will appear in the POS
                     and in your admin portal.
@@ -113,7 +148,7 @@
                                         <img
                                             src="/icons/upload.svg"
                                             alt="upload"
-                                            class="w-4 h-4"
+                                            class="w-4 h-4 my-2"
                                         />
                                         Upload (Optional)
                                     </button>
@@ -153,6 +188,14 @@
                     {/if}
                 </label>
             </div>
+        </div>
+        <div class=" fixed bottom-0 flex w-full md:hidden border-t">
+            <button
+                type="submit"
+                class="bg-purple-600 text-white w-full p-2 rounded-lg mx-5 my-4"
+            >
+                Next
+            </button>
         </div>
     </div>
 </form>
