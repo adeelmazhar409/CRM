afd45588ff0906f873fd037e53252b6e243247dc
Furqan Ahmed <98402746+furqanahmed1872@users.noreply.github.com>
2024-09-02 19:37:05 +0500

    ok

--- staff-module/src/routes/+layout.svelte
+++ staff-module/src/routes/+layout.svelte
@@ -1,28 +1,7 @@
 <script>
-<<<<<<< Updated upstream
     import '../app.css'
 </script>
 
 <div class=" max-w-full overflow-x-hidden">
     <slot />
-=======
-    import Footer from '$lib/component/footer.svelte'
-    import Header from '$lib/component/header.svelte'
-    import SideNav from '$lib/component/main/side-nav.svelte'
-    import '../app.css'
-</script>
-
-<div class="bg-white w-full h-dvh">
-    <div class="flex flex-col min-h-screen">
-        <Header />
-
-        <div class="flex flex-grow w-full">
-            <div class="bg-component-bg border-t-2 border-border-color w-1/5">
-                <SideNav />
-            </div>
-            <slot />
-        </div>
-        <Footer />
-    </div>
->>>>>>> Stashed changes
 </div>
--- staff-module/src/routes/+page.svelte
+++ staff-module/src/routes/+page.svelte
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 <div class="flex items-center justify-center text-white bg-slate-700 w-full h-full text-2xl font-extrabold">
     components render according to there page data
 </div>
 
 
-=======
-
-<div class="flex items-center w-full justify-center bg-slate-600 text-white text-2xl font-extrabold">
-    Components render here according to page
-</div>
->>>>>>> Stashed changes
